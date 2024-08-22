382d49377b68bc60c5393f0e975874a6a8195c4a
Tony Dugué <duguetony@gmail.com>
2024-08-22 13:32:37 +0200

    feat(router): add simple routes to router & create page main

--- projects/vinylcollection/src/app/app.routes.ts
+++ projects/vinylcollection/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { vinylsRoutes } from './features/vinyls/vinyls.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'main' },
+  { path: 'main', component: MainComponent },
+  ...vinylsRoutes
+];
---
+++ projects/vinylcollection/src/app/features/vinyls/vinyls.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { ListVinylsComponent } from "./components/list-vinyls/list-vinyls.component";
+
+export const vinylsRoutes: Routes = [
+  { path: 'vinyls', component: ListVinylsComponent }
+]
